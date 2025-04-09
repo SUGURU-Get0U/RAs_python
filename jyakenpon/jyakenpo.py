a035384fc15ed039b3558bd0f557d80878195199
Liam <twich.euedu@gmail.com>
2025-04-09 09:01:07 -0300

    banana

---
+++ jyakenpon/jyakenpo.py
@@ -0,0 +1,16 @@
+# Jokenpo
+
+print('Bien-venido ao jokenpo!')
+
+Player1 = input('Type in your username: ')
+
+PlayerOption = []
+BotOption = []
+PlayableOptions = [rock, paper, scissors]
+
+print('lets start the game')
+
+for item in zip(PlayableOptions):
+    print(item)
+
+playerop = input()
