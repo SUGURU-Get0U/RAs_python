8619ba77dce5be10d6ad3a65dc370dbd0fd3e3ff
Liam <twich.euedu@gmail.com>
2025-04-09 10:46:54 -0300

    banana 3.0 (bot x humano)

--- jyakenpon/jyakenpo.py
+++ jyakenpon/jyakenpo.py
@@ -2,13 +2,16 @@
 import random
 from random import randrange
 
+def victory():
+    print("YOU WON OMGA!")
+
+def lost():
+    print("O computador venceu")
+
 print('Bien-venido ao jokenpo!')
 
 Player1 = input('Type in your username: ')
 
-PlayerOption = []
-BotOption = []
-Draw = PlayerOption == BotOption
 PlayableOptions = ['rock', 'paper', 'scissors']
 
 print('lets start the game')
@@ -17,11 +20,18 @@ for item in zip([1,2,3], PlayableOptions, strict= True):
     print(item[0], ' - ', item[1])
 
 playerop = input("Choose an option: ")
-PlayerOption.append(playerop)
 
-BotOp = randrange(0,3)
-BotOption.append(BotOp)
 
-if PlayerOption == BotOption:
+BotOp = random.choice(PlayableOptions)
+
+
+if playerop == BotOp:
     print("Both chose the same option, its a draw")
 
+elif playerop == "rock" and BotOp == "paper" \
+        or playerop == "paper" and BotOp == "scissors" \
+        or playerop == "scissors" and BotOp == "rock":
+    lost()
+
+else:
+    victory()
