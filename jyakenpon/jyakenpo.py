2a12dd973cdbfce8ff0d55ec946c3a84eb753a86
Liam <twich.euedu@gmail.com>
2025-04-09 09:31:17 -0300

    banana 2.0

--- jyakenpon/jyakenpo.py
+++ jyakenpon/jyakenpo.py
@@ -1,4 +1,6 @@
-# Jokenpo
+# Jokenpon
+import random
+from random import randrange
 
 print('Bien-venido ao jokenpo!')
 
@@ -6,11 +8,20 @@ Player1 = input('Type in your username: ')
 
 PlayerOption = []
 BotOption = []
-PlayableOptions = [rock, paper, scissors]
+Draw = PlayerOption == BotOption
+PlayableOptions = ['rock', 'paper', 'scissors']
 
 print('lets start the game')
 
-for item in zip(PlayableOptions):
-    print(item)
+for item in zip([1,2,3], PlayableOptions, strict= True):
+    print(item[0], ' - ', item[1])
 
-playerop = input()
+playerop = input("Choose an option: ")
+PlayerOption.append(playerop)
+
+BotOp = randrange(0,3)
+BotOption.append(BotOp)
+
+if PlayerOption == BotOption:
+    print("Both chose the same option, its a draw")
+
