abe08b1497e89d89c4ec71b5a2242f42f3bb3175
DanielLJ06 <dani.langnerj@gmail.com>
2025-04-10 15:12:30 -0300

    jk

--- jyakenpon/jyakenpo.py
+++ jyakenpon/jyakenpo.py
@@ -1,27 +1,85 @@
 # Jokenpon
 import random
+import os
 from random import randrange
+from time import sleep
 
-print('Bien-venido ao jokenpo!')
+
+def endgame():
+    print('el juego termino')
+    sleep(3)
+    os.system('cls')
+def victory():
+    print(Player1, " chose ", playerop, "computer chose ", BotOp)
+    print('you won <3')
+
+def lost():
+    print(Player1, " chose ", playerop, "computer chose ", BotOp)
+    print('COP won :(')
+
+def bot1(botwon):
+    print(botwon + "won")
+
+
+print("Welcome!")
 
 Player1 = input('Type in your username: ')
-
-PlayerOption = []
-BotOption = []
-Draw = PlayerOption == BotOption
+modes = ['vs COP', 'vs human', 'bot vs bot']
 PlayableOptions = ['rock', 'paper', 'scissors']
+
+for mode in zip([1,2,3], modes, strict=True):
+    print(mode[0], ' - ', mode[1])
+
+typemode = input('Select the mode: ')
 
 print('lets start the game')
 
-for item in zip([1,2,3], PlayableOptions, strict= True):
-    print(item[0], ' - ', item[1])
+while typemode == 'vs COP':
 
-playerop = input("Choose an option: ")
-PlayerOption.append(playerop)
+    for item in zip([1,2,3], PlayableOptions, strict= True):
+        print(item[0], ' - ', item[1])
 
-BotOp = randrange(0,3)
-BotOption.append(BotOp)
+    playerop = input("Choose an option: ")
 
-if PlayerOption == BotOption:
-    print("Both chose the same option, its a draw")
+
+    BotOp = random.choice(PlayableOptions)
+
+
+    if playerop == BotOp:
+        print("Both chose the same option, its a draw")
+
+    elif playerop == "rock" and BotOp == "paper" \
+            or playerop == "paper" and BotOp == "scissors" \
+            or playerop == "scissors" and BotOp == "rock":
+        lost()
+
+    else:
+        victory()
+
+    endgame()
+
+
+while typemode == 'bot vs bot':
+    BotOp2 = random.choice(PlayableOptions)
+    BotOp = random.choice(PlayableOptions)
+
+    if BotOp == BotOp2:
+        print(Player1, 'and the computer chose the same option')
+        print('draw!')
+
+    elif BotOp2 == "rock" and BotOp == "paper" \
+            or BotOp2 == "paper" and BotOp == "scissors" \
+            or BotOp2 == "scissors" and BotOp == "rock":
+        bot1(botwon= 'bot 1')
+
+    elif BotOp == "rock" and BotOp2 == "paper" \
+            or BotOp == "paper" and BotOp2 == "scissors" \
+            or BotOp == "scissors" and BotOp2 == "rock":
+        bot1(botwon= 'bot 2')
+
+
+
+
+
+
 
