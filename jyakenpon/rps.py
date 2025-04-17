345bbcd849885a916e92231efde1c9cf2891f47a
Liam <dudi.cap07@gmail.com>
2025-04-17 17:44:19 -0300

    zaralhei o rps,

--- jyakenpon/rps.py
+++ jyakenpon/rps.py
@@ -1,99 +1,157 @@
 
 import random
-import subprocess
-import time
+from tkinter import *
 
-
-
-game_modes = ["VsCop", "CopVsCop", "1v1"]
-
-def tie():
-    print("O jogo terminou em empate!")
-
-def win(champion= "bug"):
-    print(champion + " venceu!")
-
-def lost(winner="bot 2", loser="bot 1"):
-    print(winner, " you lost :(", loser, " won!")
-
-def clear_terminal():
-    time.sleep(3)
-    subprocess.run('cls', shell=True, check=True)
-
-def start_game():
-    for items in zip([1,2,3], game_modes, strict=True):
-        print(items[0], "-", items[1])
-
-    ps_call = input("Select the game mode: ")
-
-def exitt():
-    saida = input("Voce deseja recomecar o jogo? [s/n]: ")
-    if saida == "s":
-        start_game()
-    else:
-        print("obrigado por jogar")
-
-# Comecar o jogo
-start = input("Voce deseja comecar o jogo? [s/n] ")
-
-if start == "s":
-    start = True
-
-else:
-    start = False
-
-
-#  saber o modo de jogo
-
-while not start:
-    start = input("Voce deseja comecar o jogo? [s/n]")
-
-
-
-# Lista de opcoes jogáveis
+# Variaveis para o jogo
 calls = ["pedra", "papel", "tesoura"]
 
-# 1 V 1
-    # 2 inputs p1 e p2
-    # analisar se os inputs sao validos
-    # ver quem ganhou
+turn = 1
+# UI (user interface)
+# Cria uma janela
+tela = Tk()
+tela.geometry("200x200")
+tela.title("Pedra Papel e Tesoura")
+Game_title = Label(tela, text="Pedra Papel e Tesoura")
+Game_title.grid(column=0, row=0)
+
+game_mode = Label(tela, text= "")
+# Cria a resposta ao botão play, o texto será alterado pela funcao game_menu
+
+def start_game():
+    global turn, game_mode
+    turn = 1
+    # Modo de jogo Player vs Cop
+    if game_mode["text"] == "1":
+
+        while game_mode["text"] == "1":
+            print(f"Rodada {turn}")
+            p1 = input("Faca sua escolha: ")
+            cop1 = random.choice(calls)
+            if p1 not in calls:
+                print("Input do jogador invalido. \nPor favor escolha entre Pedra,papel ou tesoura")
+                p1 = input("Faca sua escolha: ")
+            else:
+                continue
+            print(f"A escolha do jogador foi: {p1}\n")  # Mostra a escolha do player
+            print(f"A escolha do computador foi: {cop1}\n")
+            verifica(p1, cop1)
+            turn += 1  # Passa para proxima rodada
+            if turn == 5:  # Ao final de 5 rodadas
+                res = input("quer recomeçar em outro modo?? [s/n]: ")
+                if res.lower() == ['s','sim']:
+                    game_menu()
+
+                else:
+                    turn = 1
+            else:
+                continue
+    elif game_mode["text"] == "2":
+    # Modo de Jogo Cop vs Cop
+        while game_mode["text"]== "2":
+            print(f"Rodada {turn}")  # Mostra a rodada atual
+            b1 = random.choice(calls)  # Jogada do bot1
+            b2 = random.choice(calls)  # Jogada do bot2
+            print(f"A escolha do bot 1 foi: {b1}\n")  # Mostra a escolha do bot1
+            print(f"A escolha do bot 2 foi: {b2}\n")  # Mostra a escolha do bot2
+            verifica(b1, b2)  # Chama a funcao para ver quem ganhou
+            turn += 1  # Passa para proxima rodada
+            if turn == 5:  # Ao final de 5 rodadas
+                res = input("quer recomeçar em outro modo?? [s/n]: ")
+                if res.lower() == ["s","sim"]:
+                    game_menu()
+
+                else:
+                    turn = 1
+            else:
+                continue
+    else:
+    # Modo de jogo Player vs Player
+        while game_mode['text'] == "3":
+            print(f"Rodada {turn}")
+            p1_call = input("Jogador 1 faca sua escolha: ")
+            p2_call = input("Jogador 2 faca sua escolha: ")
+            while p1_call not in calls or p2_call not in calls:  # Nao tem como saber quem teve o input invalido
+                print("Input invalido")
+                p1_call = input("Jogador 1 faca sua escolha: ")
+                p2_call = input("Jogador 2 faca sua escolha: ")
+            print(f"A escolha do jogador 1foi: {p1_call}\n")  # Mostra a escolha do player
+            print(f"A escolha do jogador 2 foi: {p2_call}\n")
+            verifica(p1_call, p2_call)
+            turn += 1  # Passa para proxima rodada
+            if turn == 5:  # Ao final de 5 rodadas
+                res = input("quer recomeçar em outro modo?? [s/n]: ")
+                if res.lower() == ["s","sim"]:
+                    game_menu()
+
+                else:
+                    turn = 1
+            else:
+                continue
+def select_mode(mode):
+    global modo_jogo
+    modo_de_jogo = mode
+    game_mode.config(text= f"O modo de jogo selecionado foi: {mode}")
+    # seleciona o modo de jogo
+    start_game() #comeca o jogo
+    #tira os botoes de modo de jogo depois da selecao
+    for widget in tela.winfo_children():
+        if isinstance(widget, Button) and widget != Play_button:
+            widget.destroy()
+    # "Para cada elemento dentro da tela,
+    # se esse elemento for um botão e não for o botão 'Play', então destrua esse elemento."
+
+# Funcao cria botoes para cada modo de jogo
+def game_menu():
+    menu_options = {
+        "1": "Versus Computador",
+        "2": "Computador vs Computador",
+        "3": "Player vs Player"
+    }
+    linha = 2
+    for key, value in menu_options.items():
+        button = Button(tela, text=value, command=lambda m=key: select_mode(m))
+        button.grid(column=0, row=linha,padx=10,pady=10)
+        linha += 1
+    Play_button.grid_forget() # Remove o "Play apos o clique"
+
+# Cria um botão que executa a funcao game_menu ao ser clicado
+Play_button = Button(tela, text="Play", command=game_menu)
+Play_button.grid(column=0, row=2)
+Play_button.place(relx=0.5,rely=0.5,anchor=CENTER)
 
 
-# vs cop
-    # 1 ‘input’ + jogada do computador
-while ps_call == game_modes[0] or 0:
-    Solop1 = input("")
-    # verificao de inputs
-    # ver quem ganhou
-# cop vs cop
-while ps_call == game_modes[1] or "2":
-    b1 = random.choice(calls)
-    b2 = random.choice(calls)
-    print(f"A escolha do bot 1 foi: {b1}\n")
-    print(f"A escolha do bot 2 foi: {b2}\n")
-    # ver quem ganhou
-    # empate
+
+# Deixa a janela rodando para sempre ate alguém fechar
+tela.mainloop()
+
+
+
+# Funcao da condicao de vitoria
+def verifica(b1,b2):
     if b1 == b2:
-        tie()
-        exitt()
-
+        return print("empatou")
     # vitoria do b1
     elif b1 == calls[0] and b2 == calls[2] \
             or b1 == calls[1] and b2 == calls[0] \
             or b1 == calls[2] and b2 == calls[1]:
-        win("bot 1")
-        exitt()
-
+        return print("bot1")
     # vitoria do b2
     else:
-        lost()
-        exitt()
-    # sair do loop while
+        return print("bot2")
+
+
+def end_game(mode,run):
+    if run == 5:
+        return
+
+# Comecar o jogo
 
 
 
 
 
 
+        # sair do loop while
 
 #
+
--- jyakenpon/rps2.py
+++ jyakenpon/rps2.py
@@ -1,20 +1,55 @@
 
 import random
-import subprocess
-import time
 
-game_modes = ["VsCop", "CopVsCop", "1v1"]
+import requests
+
+# Pega todos os codigos da biblioteca
+from tkinter import *
+
+
 # Lista de opcoes jogáveis
 calls = ["pedra", "papel", "tesoura"]
 
+# UI (user interface)
+tela = Tk()  # Cria um janela
+tela.title("minecraft 2")
+Game_title = Label(tela, text="Pedra Papel e Tesoura")
+    # Cria um texto inicial na tela.
+    # Precisa de 2 atributos (A janela que será exibida, um texto)
+
+# Funcao do Menu do jogo
 def game_menu():
     menu_options = ["1","2","3","VsCop","CopVsCop","1v1"]
     print("(1) VsCop \n(2) CopVsCop \n(3) 1v1")
     x = 0
     while x not in menu_options:
-          x = input("Select the game mode: ")
+        x = input("Select the game mode: ")
     return x
 
+
+
+
+Game_title.grid(column=0, row=0)
+# Exibe o texto usando os atributos (coluna, linha)
+# como se fosse no Excel
+
+Play_button = Button(tela, text="Play", command=game_menu())
+# Cria um botão que executa funcao game_menu
+
+Play_button.grid(column=0, row=2)
+# Posiciona o Botao recém criado
+
+game_mode = Label(tela, text= "")
+# Cria a resposta ao botão play, o texto será alterado pela funcao game_menu
+
+tela.mainloop()  # Deixa a janela rodando para sempre ate alguém fechar
+
+choice =game_menu()
+
+
+
+
+# Funcao da condicao de vitoria
 def verifica(b1,b2):
     if b1 == b2:
         return print("empatou")
@@ -32,35 +67,77 @@ def end_game(mode,run):
     if run == 5:
         return
 
-
 # Comecar o jogo
 
-start = input("Voce deseja comecar o jogo? [s/n] ")
-if start == "s":
-  choice = game_menu()
-else:
-  exit()
+
 
 turn = 1
+
+# Modo de jogo Player vs Cop
+while choice == "1":
+    print(f"Rodada {turn}")
+    p1 = input("Faca sua escolha: ")
+    cop1 = random.choice(calls)
+    if p1 not in calls:
+        print("Input do jogador invalido. \nPor favor escolha entre Pedra,papel ou tesoura")
+        p1 = input("Faca sua escolha: ")
+    else:
+        continue
+    print(f"A escolha do jogador foi: {p1}\n")  # Mostra a escolha do player
+    print(f"A escolha do computador foi: {cop1}\n")
+    verifica(p1,cop1)
+    turn += 1 # Passa para proxima rodada
+    if turn == 5:  # Ao final de 5 rodadas
+        res = input("quer recomeçar em outro modo?? [s/n]: ")
+        if res == 's' or 'sim':
+            game_menu()
+        else:
+            turn = 1
+    else:
+        continue
+# Modo de Jogo Cop vs Cop
 while choice == "2":
-        print(f"Rodada {turn}")
-        b1 = random.choice(calls)
-        b2 = random.choice(calls)
-        print(f"A escolha do bot 1 foi: {b1}\n")
-        print(f"A escolha do bot 2 foi: {b2}\n")
-        verifica(b1,b2)
-        turn += 1
-        if turn == 5:
-            res = input("quer recomecar em outro mode?? s/n")
-            if res == 's':
+        print(f"Rodada {turn}") # Mostra a rodada atual
+        b1 = random.choice(calls) # Jogada do bot1
+        b2 = random.choice(calls) # Jogada do bot2
+        print(f"A escolha do bot 1 foi: {b1}\n") # Mostra a escolha do bot1
+        print(f"A escolha do bot 2 foi: {b2}\n") # Mostra a escolha do bot2
+        verifica(b1,b2) # Chama a funcao para ver quem ganhou
+        turn += 1 # Passa para proxima rodada
+        if turn == 5: # Ao final de 5 rodadas
+            res = input("quer recomeçar em outro modo?? [s/n]: ")
+            if res == 's' or 'sim':
                 game_menu()
             else:
                 turn = 1
         else:
             continue
 
+        # Modo de jogo Player vs Player
+# Modo de Jogo Player vs Player
+while choice == "3":
+    print(f"Rodada {turn}")
+    p1_call = input("Jogador 1 faca sua escolha: ")
+    p2_call = input("Jogador 2 faca sua escolha: ")
+    while p1_call not in calls or p2_call not in calls:  # Nao tem como saber quem teve o input invalido
+        print("Input invalido")
+        p1_call = input("Jogador 1 faca sua escolha: ")
+        p2_call = input("Jogador 2 faca sua escolha: ")
+    print(f"A escolha do jogador 1foi: {p1_call}\n")  # Mostra a escolha do player
+    print(f"A escolha do jogador 2 foi: {p2_call}\n")
+    verifica(p1_call, p2_call)
+    turn += 1  # Passa para proxima rodada
+    if turn == 5:  # Ao final de 5 rodadas
+        res = input("quer recomeçar em outro modo?? [s/n]: ")
+        if res == 's' or 'sim':
+            game_menu()
+        else:
+            turn = 1
+    else:
+        continue
 
 
         # sair do loop while
 
 #
+
