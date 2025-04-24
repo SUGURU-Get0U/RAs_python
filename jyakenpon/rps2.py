4fccc19607b8119831ff3971a48db3df14452bdf
Liam <dudi.cap07@gmail.com>
2025-04-23 22:09:23 -0300

    versao definitiva

--- jyakenpon/rps2.py
+++ jyakenpon/rps2.py
@@ -6,7 +6,7 @@ import time
 
 
 # Lista de opcoes jogáveis
-calls = ["pedra [0] \n","papel [1]\n", "tesoura [2]"]
+calls = ["1", "2", "3", "pedra","papel", "tesoura"]
 
 
 # Funcao do Menu do jogo
@@ -30,9 +30,9 @@ def verifica(b1,b2):
     if b1 == b2:
         return print("empatou")
     # vitoria do b1
-    elif b1 == calls[0] and b2 == calls[2] \
-            or b1 == calls[1] and b2 == calls[0] \
-            or b1 == calls[2] and b2 == calls[1]:
+    elif b1 == calls[3] and b2 == calls[5] \
+            or b1 == calls[4] and b2 == calls[3] \
+            or b1 == calls[5] and b2 == calls[4]:
         return print("O player 2 venceu")
     # vitoria do b2
     else:
@@ -52,21 +52,26 @@ turn = 1
 # Modo de jogo Player vs Cop
 while choice == "1":
     print(f"Rodada {turn}")
-    print(*calls)
-    p1 = input("Faca sua escolha: ")
-    cop1 = random.choice(calls)
-    if p1 not in calls:
-        print("Input do jogador invalido. \nPor favor escolha entre Pedra,papel ou tesoura")
-        p1 = input("Faca sua escolha: ")
+    print("pedra [1] ","papel [2]", "tesoura [3]")
+    p1_num = input("Faca sua escolha: ")
+    if p1_num == '1' or 1:
+        p1 = calls[3]
+    elif p1_num == '2' or 2:
+        p1 = calls[4]
+    elif p1_num == '3' or 3:
+        p1 = calls[5]
     else:
+        print("Input do jogador invalido. \nPor favor escolha entre Pedra,papel ou tesoura")
         continue
+    cop1 = random.choice(calls[3:])
+
     print(f"A escolha do jogador foi: {p1}\n")  # Mostra a escolha do player
     print(f"A escolha do computador foi: {cop1}\n")
     verifica(p1,cop1)
     turn += 1 # Passa para proxima rodada
     if turn == 5:  # Ao final de 5 rodadas
         res = input("quer recomeçar em outro modo?? [s/n]: ")
-        if res == 's' or 'sim':
+        if res.lower() == 's' or 'sim':
             game_menu()
         else:
             turn = 1
@@ -95,20 +100,32 @@ while choice == "2":
 # Modo de Jogo Player vs Player
 while choice == "3":
     print(f"Rodada {turn}")
-    print(*calls)
+    print("pedra [1] ", "papel [2]", "tesoura [3]")
     p1_call = input("Jogador 1 faca sua escolha: ")
+    if p1_call == '1' or 1:
+        p1 = calls[3]
+    elif p1_call == '2' or 2:
+        p1 = calls[4]
+    elif p1_call == '3' or 3:
+        p1 = calls[5]
     p2_call = input("Jogador 2 faca sua escolha: ")
+    if p2_call == '1' or 1:
+        p1 = calls[3]
+    elif p2_call == '2' or 2:
+        p1 = calls[4]
+    elif p2_call == '3' or 3:
+        p1 = calls[5]
     while p1_call not in calls or p2_call not in calls:  # Nao tem como saber quem teve o input invalido
         print("Input invalido")
         p1_call = input("Jogador 1 faca sua escolha: ")
         p2_call = input("Jogador 2 faca sua escolha: ")
-    print(f"A escolha do jogador 1foi: {p1_call}\n")  # Mostra a escolha do player
+    print(f"A escolha do jogador 1 foi: {p1_call}\n")  # Mostra a escolha do player
     print(f"A escolha do jogador 2 foi: {p2_call}\n")
     verifica(p1_call, p2_call)
     turn += 1  # Passa para proxima rodada
     if turn == 5:  # Ao final de 5 rodadas
         res = input("quer recomeçar em outro modo?? [s/n]: ")
-        if res == 's' or 'sim':
+        if res.lower() == 's' or 'sim':
             game_menu()
         else:
             turn = 1
